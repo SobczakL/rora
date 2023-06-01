7836082be27d25852c907f56ab40e5073754b405
lucqaz01 <Lucassobczak3@gmail.com>
2023-06-01 16:35:59 -0400

    Changed server url to new vercel url

--- client/src/services/config.js
+++ client/src/services/config.js
@@ -1,4 +1,4 @@
-export const serverURL = "http://localhost:8080";
+export const serverURL = "https://rora-server.vercel.app/";
 export const apiURL = "https://external.transitapp.com/v3";
 export const apiKey =
     "7a24d152ab61c207fb15f519b96e64026bdcc20a6eb6c5cb215386c9438f7109";
