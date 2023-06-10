595eb5a9f183657bf3629be7db6c2d2f79b7d81b
lucqaz01 <Lucassobczak3@gmail.com>
2023-06-10 17:41:04 -0400

    editted userDetails and editUserDetails to express from knex

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -19,47 +19,49 @@ exports.userLogin = async (req, res) => {
         } else {
             res.status(401).json({ error: "INCORRECT_PASSWORD" });
         }
-    } catch (err) {
-        console.error(err);
+    } 
+    catch (err) {
         res.status(500).json({ error: "INTERNAL_SERVER_ERROR" });
     }
 };
 
-// exports.getUserDetails = (req, res) => {
-//     const { username } = req.body;
-//     knex("userData")
-//         .where("username", username)
-//         .select("*")
-//         .then((rows) => {
-//             if (rows.length === 0) {
-//                 res.status(404).send("No user data found.");
-//             } else {
-//                 res.status(200).json(rows);
-//             }
-//         })
-//         .catch((err) => {
-//             res.status(500).send(err.error);
-//         });
-// };
-//
-// exports.editUserDetails = (req, res) => {
-//     const { username, data } = req.body;
-//     const newUserDetails = {
-//         firstName: data.firstName,
-//         LastName: data.LastName,
-//         email: data.email,
-//         phone: data.phone,
-//         cardNumber: data.cardNumber,
-//         exDate: data.exDate,
-//         cvc: data.cvc,
-//         zip: data.zip,
-//     };
-//     knex("userData")
-//         .where("username", username)
-//         .update(newUserDetails)
-//         .then((response) => {
-//             res.status(200).send("User details saved.");
-//         })
-//         .catch((err) => {
-//             res.status(500).send(err.error);
-//         });
+exports.getUserDetails = async(req, res) => {
+    const { username } = req.body;
+
+    try {
+      const query = 'SELECT * FROM userData WHERE username = ?';
+      const [rows] = await connection.promise().query(query, [username]);
+
+      if (rows.length === 0) {
+        res.status(404).send("No user data found.");
+      } 
+      else {
+        res.status(200).json(rows);
+      }
+
+    } 
+    catch (err) {
+      res.status(500).send(err.error);
+    } 
+};
+
+exports.editUserDetails = async(req, res) => {
+    const { username, data } = req.body;
+    const newUserDetails = {
+        first_name: data.firstName,
+        last_name: data.lastName,
+        email: data.email,
+        phone: data.phone,
+        card_number: data.cardNumber,
+        ex_date: data.exDate,
+        cvc: data.cvc,
+        zip: data.zip,
+    };
+  try {
+    const query = 'UPDATE userData SET ? WHERE username = ?';
+    await connection.promise().query(query, [newUserDetails, username]);
+  } 
+  catch (err) {
+    res.status(500).send(err.error); 
+  }
+};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const userController = require("../controllers/userController");
 
 router.route("/").post(userController.userLogin);
-// router.route("/userDetails").post(userController.getUserDetails);
-// router.route("/editUserDetails").post(userController.editUserDetails);
+router.route("/userDetails").post(userController.getUserDetails);
+router.route("/editUserDetails").post(userController.editUserDetails);
 
 module.exports = router;
